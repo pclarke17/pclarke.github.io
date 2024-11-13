86c6a57fdadab18e5648bbcf243dd7a18da11d5c
pclarke17 <p.clarke.2018@alumnos.urjc.es>
2024-11-13 12:33:32 +0100

    cambios

--- Video/video-texture-component.js
+++ Video/video-texture-component.js
@@ -6,6 +6,7 @@ AFRAME.registerComponent('video-canvas-texture', {
   init: function () {
     // Obtener el elemento de video
     this.videoElement = document.getElementById('local-video');
+    console.log("Elemento de video obtenido:", this.videoElement);
 
     // Verificar si el video se obtuvo correctamente
     if (!this.videoElement) {
@@ -16,12 +17,14 @@ AFRAME.registerComponent('video-canvas-texture', {
     // Crear un canvas para dibujar el contenido del video
     this.canvas = document.createElement('canvas');
     this.context = this.canvas.getContext('2d');
+    console.log("Canvas creado con contexto 2D:", this.canvas, this.context);
 
     // Crear una textura a partir del canvas y asignarla al material de la caja
     this.texture = new THREE.Texture(this.canvas);
     const mesh = this.el.getObject3D('mesh');
     if (mesh) {
       mesh.material.map = this.texture;
+      console.log("Textura creada y asignada al material del objeto 3D.");
     } else {
       console.error('No se encontró el mesh del elemento al cual aplicar la textura.');
       return;
@@ -29,7 +32,10 @@ AFRAME.registerComponent('video-canvas-texture', {
 
     // Obtener acceso a la cámara del ordenador (si es necesario) o cargar el video
     this.videoElement.addEventListener('loadeddata', () => {
+      console.log("Datos del video cargados.");
+
       if (this.videoElement.readyState >= this.videoElement.HAVE_CURRENT_DATA) {
+        console.log("El video está listo, configurando el canvas.");
         // Configurar el tamaño del canvas basado en el tamaño del video
         this.canvas.width = this.videoElement.videoWidth;
         this.canvas.height = this.videoElement.videoHeight;
@@ -50,6 +56,8 @@ AFRAME.registerComponent('video-canvas-texture', {
   updateCanvas: function () {
     // Verificar si el video tiene suficientes datos para ser dibujado
     if (this.videoElement.readyState >= this.videoElement.HAVE_ENOUGH_DATA) {
+      console.log("Dibujando el video en el canvas.");
+
       // Dibujar el frame del video en el canvas
       this.context.drawImage(this.videoElement, 0, 0, this.canvas.width, this.canvas.height);
 
@@ -76,6 +84,9 @@ AFRAME.registerComponent('video-canvas-texture', {
 
       // Marcar la textura para actualizarla
       this.texture.needsUpdate = true;
+      console.log("Textura actualizada con los datos del canvas.");
+    } else {
+      console.warn("El video aún no tiene suficientes datos para ser dibujado.");
     }
 
     // Continuar actualizando el canvas en el siguiente frame
