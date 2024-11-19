d657eb8d55d097534566d171a8e1118e41f0f4c3
pclarke17 <p.clarke.2018@alumnos.urjc.es>
2024-11-19 19:39:56 +0100

    cc

--- Cmara-webrtc/peer-server.js
+++ Cmara-webrtc/peer-server.js
@@ -40,41 +40,43 @@ AFRAME.registerComponent('peer-connection', {
 
   initReceiver: function () {
     const peerID = this.data.peerId;
-
+  
     if (!peerID) {
       console.error('El ID del Peer proporcionado es inválido o está vacío.');
       return;
     }
-
+  
     console.log(`Receptor: Intentando conectar al Peer ID del transmisor: ${peerID}`);
-
+  
     this.peer.on('open', () => {
       console.log('Receptor: Conexión abierta con PeerJS, intentando conectar con el transmisor.');
-
+  
       const call = this.peer.call(peerID, null);
-
+  
       if (!call) {
         console.error("Error al realizar la llamada: el objeto 'call' no fue creado. Verifica el Peer ID del transmisor.");
+        alert("Error en la conexión. Verifica el Peer ID ingresado e inténtalo de nuevo.");
         return;
       }
-
+  
       call.on('stream', (remoteStream) => {
         console.log('Receptor: Recibido el stream desde el transmisor.');
         const videoElement = document.createElement('video');
         videoElement.srcObject = remoteStream;
         videoElement.autoplay = true;
         videoElement.playsInline = true;
-
-        document.body.appendChild(videoElement); // Agregar video al documento
+  
+        document.body.appendChild(videoElement);
       });
-
+  
       call.on('error', (err) => {
         console.error('Error en la llamada de PeerJS:', err);
       });
     });
-
+  
     this.peer.on('error', (err) => {
       console.error('Error en PeerJS:', err);
     });
   }
+  
 });
